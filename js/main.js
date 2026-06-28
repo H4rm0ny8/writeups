c19363f23c134c2a7a8fa1c8619dc024531afb48
H4rm0ny <ikramimadi@fci.bu.edu.eg>
2026-06-28 08:38:11 +0300

    update
    cards

--- js/main.js
+++ js/main.js
@@ -387,7 +387,7 @@ if (motionOk) {
       const rect = card.getBoundingClientRect();
       const x = (e.clientX - rect.left) / rect.width - 0.5;
       const y = (e.clientY - rect.top) / rect.height - 0.5;
-      card.style.transform = `translateY(-4px) perspective(400px) rotateY(${x * 4}deg) rotateX(${-y * 4}deg)`;
+      card.style.transform = `translateX(6px) perspective(400px) rotateY(${x * 4}deg) rotateX(${-y * 4}deg)`;
     });
     card.addEventListener("mouseleave", () => {
       card.style.transform = "";
